75999da76def14bb46b81bfe342e96feca52fe25
kashaf javaid <kashafjavaid@hotmail.com>
2024-01-29 23:26:11 -0800

    isdooropen function added in this commit

--- main.cpp
+++ main.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
 class Door {
 private:
     bool isopen;
     bool islocked;
-
 public:
-    Door();
-    void opendoor();
-    void closedoor();
-    void lockdoor();
-    void unlockdoor();
-    bool isdooropen() const;
-    bool isdoorlocked() const;
+    Door() : isopen(false), islocked(false) {}
+ 
+    bool isdooropen() const {
+        return isopen;
+    }
 };
-
-
 int main() {
     cout << "Hello World" << endl;
-
     // You can create an instance of the Door class and use its methods here.
-
     return 0;
 }
+Change isdooropenand isdoorlocked function
